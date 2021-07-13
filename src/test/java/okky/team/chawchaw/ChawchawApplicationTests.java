db9e5e0d7204ffe2552d5781c97cf879fc04b5e3
HaJooHyun <mangchhe@naver.com>
2021-07-13 14:51:15 +0900

    Init Project

---
+++ src/test/java/okky/team/chawchaw/ChawchawApplicationTests.java
@@ -0,0 +1,13 @@
+package okky.team.chawchaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChawchawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
