6b482e6a0b53961b5451dec9a297df8ae0f69c77
HaJooHyun <mangchhe@naver.com>
2021-07-25 13:41:57 +0900

    Add: schema.sql [데이터베이스 DDL], application-prod.yml [배포 환경 설정 파일]

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,77 @@
+create table country
+(
+    country_id bigint auto_increment
+        primary key,
+    name       varchar(255) null
+);
+
+create table language
+(
+    language_id bigint auto_increment
+        primary key,
+    abbr        varchar(255) null,
+    name        varchar(255) null
+);
+
+create table users
+(
+    user_id       bigint auto_increment
+        primary key,
+    content       varchar(2000) null,
+    email         varchar(255)  not null,
+    facebook_url  varchar(255)  null,
+    image_url     varchar(1000) null,
+    instagram_url varchar(255)  null,
+    name          varchar(255)  not null,
+    password      varchar(255)  not null,
+    reg_date      datetime(6)   not null,
+    role          varchar(255)  not null,
+    school        varchar(255)  not null,
+    views         bigint        not null,
+    web_email     varchar(255)  not null,
+    unique (email)
+);
+
+create table follow
+(
+    follow_id bigint auto_increment
+        primary key,
+    reg_date  datetime(6) not null,
+    user_from bigint      null,
+    user_to   bigint      null,
+    foreign key (user_to) references users (user_id),
+    foreign key (user_from) references users (user_id)
+);
+
+create table user_country
+(
+    user_country_id bigint auto_increment
+        primary key,
+    country_id      bigint null,
+    user_id         bigint null,
+    foreign key (country_id) references country (country_id),
+    foreign key (user_id) references users (user_id)
+);
+
+create table user_language
+(
+    user_language_id bigint auto_increment
+        primary key,
+    language_id      bigint null,
+    user_id          bigint null,
+    foreign key (user_id) references users (user_id),
+    foreign key (language_id) references language (language_id)
+);
+
+create table user_hope_language
+(
+    user_hope_language_id bigint auto_increment
+        primary key,
+    language_id           bigint null,
+    user_id               bigint null,
+    foreign key (user_id) references users (user_id),
+    foreign key (language_id) references language (language_id)
+);
+
+
+
