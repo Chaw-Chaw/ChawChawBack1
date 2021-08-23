fb9ac6c16b8ce316bfd964f45fee2d8c0c94ec9c
HaJooHyun <mangchhe@naver.com>
2021-08-24 02:44:54 +0900

    Update: JwtAuthorizationFilter [잘못된 인가 정보 상태 코드 403 -> 401], ResponseAuthMessage [추후 RefreshToken을 위한 메시지 변경]

--- src/main/java/okky/team/chawchaw/utils/message/ResponseAuthMessage.java
+++ src/main/java/okky/team/chawchaw/utils/message/ResponseAuthMessage.java
@@ -2,7 +2,7 @@ package okky.team.chawchaw.utils.message;
 
 public class ResponseAuthMessage {
 
-    public static final String EXPIRE_TOKEN = "JWT 토큰 만료";
-    public static final String WRONG_TOKEN_FORM = "JWT 토큰 형태가 잘못됨";
+    public static final String EXPIRE_ACCESS_TOKEN = "Access Token 만료";
+    public static final String WRONG_ACCESS_TOKEN_FORM = "Access Token 형태가 잘못됨";
 
 }
